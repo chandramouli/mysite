3b99e6e88eda442f7da401c11fcf9a39886f7f31
unknown <cgajendiran@.sagework.com>
2011-12-02 13:04:10 +0530

    first commit

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,13 @@
+class HomesController < ApplicationController
+  def index
+  end
+
+  def business_page
+  end
+
+  def personal_page_index 	
+  end
+  
+  def personal_page
+  end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySite::Application.config.secret_token = '973ae6bc3613a5ed4fd918a783c5ae6f59c527fcf4998f86e35a8ddf446a39a8b6e42a0b676ec631c18429e3a8cafa3d9b583f6b834c1bfb51febf61d1eceef2'
